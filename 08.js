8abbd8df22d01f840fb07706ffa6cadb1a95fa10
Kostya <goncharovk911@gmail.com>
2019-12-24 00:51:03 +0200

    NewEvenNumbers

--- 08.js
+++ 08.js
@@ -1,16 +1,4 @@
 var arg = [ 20, 11,  6, 33, 25, 54, 97], newArg=[]
 
-var assert = (arg) => {
-    if (arg.length>0) {
-        var argNumb = arg.shift();
-        if (argNumb%2) {
-            assert(arg)         
-        }else{
-            newArg.push(argNumb)
-        }
-        assert(arg)
-    }return newArg
-}
-
-assert(arg);
-console.log(newArg);
+var result = arg.filter(arg => arg % 2 == 0);
+console.log(result);
